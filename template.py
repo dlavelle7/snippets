b29151718504c0b829433f6a9f6a37d76d0d70b4
David Lavelle <d.lavelle7@yahoo.ie>
2015-12-18 12:21:13 +0000

    initial commit.;

---
+++ template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import time
+import sys
+
+def main():
+    while True:
+        print "Interrupt me!"
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print 'Keyboard Interrupt, exiting . . .'
+        sys.exit(1)
+    except Exception as e:
+        print 'Something went wrong, exiting . . .'
+        sys.exit(1)
