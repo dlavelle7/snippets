ad6caa1fc70bcabdc91cf48ca1320006aab47e4d
David Lavelle <d.lavelle7@yahoo.ie>
2015-12-22 13:09:45 +0000

    Argparse example.

--- python/argparsing.py
+++ python/argparsing.py
@@ -4,18 +4,25 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
     # Positional argument
-    parser.add_argument("square",
-            help="prints square root of positional arg 1", type=int)
+    parser.add_argument("base", help="base value", type=int)
     # Optional argument
-    parser.add_argument("-v", "--verbose", help="increased output verbosity",
+    parser.add_argument("-e", "--expodent", help="expodent value (defaults to 2)",
+            type=int, default=2, choices=[0, 1, 2, 3, 4])
+    group.add_argument("-v", "--verbose", help="increased output verbosity",
+            action="store_true")
+    group.add_argument("-q", "--quiet", help="minimum output verbosity",
             action="store_true")
     args = parser.parse_args()
-    answer = args.square**2
+    answer = args.base**args.expodent
     if args.verbose:
-        print "The square root of {0} is {1}".format(args.square, answer)
-    else:
+        print "{0} to the power of {1} equals {2}".format(
+                args.base, args.expodent, answer)
+    elif args.quiet:
         print answer
+    else:
+        print "{0}^{1} == {2}".format(args.base, args.expodent, answer)
 
 
 if __name__ == "__main__":
