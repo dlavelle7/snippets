e1c7533ff0fd487000c50d2cadf44079d6f4df83
David Lavelle <d.lavelle7@yahoo.ie>
2016-03-24 10:30:01 +0000

    Improved reverse walk and added thread context manager.

--- python/reverse_walk.py
+++ python/reverse_walk.py
@@ -5,23 +5,39 @@ import sys
 
 
 def get_repo_root_dir():
-    """Find root directory of current git repo
+    """Return the root directory of current git repo
 
     If the current working directory is not in a git repo, print error and
     exit. Assuming that a git repo will always contain a .git directory
     and .gitignore / README.md files.
     """
-    root_dirs = ['.git']
-    root_files = ['.gitignore', 'README.md']
+    root_nodes = ['.gitignore', 'README.md', '.git']
     current = os.getcwd()
     while current != "/":
         allnames = os.listdir(current)
-        if all(root_dir in allnames for root_dir in root_dirs) and \
-                all(root_file in allnames for root_file in root_files):
+        if all(root_node in allnames for root_node in root_nodes):
             return current
         else:
             current = os.path.abspath(os.path.join(current, os.pardir))
     print "The current working directory is not a git repository."
     sys.exit(1)
 
+
+def reverse_walk(bottom):
+    """Walk up the directory tree and yield (dirpath, dirnames, filenames)
+
+    Similar to os.walk() in reverse, but do not recurse over the
+    subdirectories, just iterate back until root.
+    """
+    while bottom != "/":
+        names = os.listdir(bottom)
+        dirs = [f for f in names if os.path.isdir(os.path.join(bottom, f))]
+        files = [f for f in names if os.path.isfile(os.path.join(bottom, f))]
+        yield bottom, dirs, files
+        bottom = os.path.abspath(os.path.join(bottom, os.pardir))
+
+
 print get_repo_root_dir()
+
+for dirpath, dirnames, filenames in reverse_walk(os.getcwd()):
+    print dirpath, dirnames, filenames
---
+++ python/thread_guard_context_manager.py
@@ -0,0 +1,34 @@
+import threading
+import time
+
+
+class ThreadGuard(object):
+    """Threading context manager which ensures all threads are finished."""
+
+    def __init__(self):
+        self.enter_threads = set()
+
+    def __enter__(self):
+        self.enter_threads.update(thread for thread in threading.enumerate())
+
+    def __exit__(self, *args):
+        exit_threads = set(thread for thread in threading.enumerate())
+        leftover_threads = exit_threads - self.enter_threads
+        for thread in leftover_threads:
+            thread.join()
+
+
+def foo(thread_id):
+    time.sleep(3)
+    print "Thread {0} finished".format(thread_id)
+
+t1 = threading.Thread(target=foo, args=[1])
+t2 = threading.Thread(target=foo, args=[2])
+
+thread_guard = ThreadGuard()
+
+with thread_guard:
+    t1.start()
+    t2.start()
+
+print "Main thread done"
