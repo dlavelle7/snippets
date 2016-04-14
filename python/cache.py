23230f75271e2b988303219786ea754ada50203a
David Lavelle <d.lavelle7@yahoo.ie>
2016-04-14 17:39:57 +0100

    cached fibonacci decorator.

--- python/cache.py
+++ python/cache.py
@@ -3,15 +3,16 @@
 
 def cache_fibs(callable_func):
     cache = dict()
+
     def wrapper(n):
         if n not in cache:
             cache[n] = callable_func(n)
         return cache[n]
+
     return wrapper
-        
 
 
-#@cache_fibs
+@cache_fibs
 def fib(n):
     """Return nth fibonacci number"""
     if n < 2:
@@ -19,4 +20,6 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 
-print fib(4)
+true_fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+
+assert [fib(n) for n in range(len(true_fib))] == true_fib
