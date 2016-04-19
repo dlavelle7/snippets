2e936ca1b213fb5ab5e0c17156ee59ec329ae64b
David Lavelle <d.lavelle7@yahoo.ie>
2016-04-19 11:36:15 +0100

    more readable fib function.

--- python/cache.py
+++ python/cache.py
@@ -2,12 +2,11 @@
 """
 Caching decorator for storing fibonacci numbers.
 
-Particularly useful here due to the recursive nature of the fib() function.
-Measure execution time with 'timeit' library. Output compared without use of
-the @cache decorator:
+Measure execution time with 'timeit' library. For example, output compared
+without use of the @cache decorator:
 
-With decorator      : < 1 sec
-Without decorator   : 43 secs
+With decorator      : 0.0364 secs
+Without decorator   : 1.9738 secs
 """
 import timeit
 
@@ -22,27 +21,27 @@ def cache_fibs(callable_func):
     return wrapper
 
 
-@cache_fibs
 def fib(n):
     """Return nth fibonacci number"""
-    if n < 2:
-        return n
-    val1 = fib(n-1)
-    val2 = fib(n-2)
-    return val1 + val2
+    sequence = [0, 1]
+    if n < len(sequence):
+        return sequence[n]
+    while len(sequence) <= n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence.pop()
 
 
-# Test fib() function
-true_fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
-assert [fib(n) for n in range(len(true_fib))] == true_fib
-
-
-# Measure execution time with timeit library
+# Measure execution time
 def wrapper(func, *args, **kwargs):
     def wrapped():
         return func(*args, **kwargs)
     return wrapped
 
-wrapped_func = wrapper(fib, 40)
+wrapped_func = wrapper(fib, 100)
 
-print "Execution time: %s" % timeit.timeit(wrapped_func, number=1)
+print "Execution time: %s" % timeit.timeit(wrapped_func, number=100000)
+
+
+# Test fib() function
+true_fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+assert [fib(n) for n in range(len(true_fib))] == true_fib
