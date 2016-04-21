7f12fc0a27f34f35fffdeeb2c8b7a3b1af97f854
David Lavelle <d.lavelle7@yahoo.ie>
2016-04-21 13:03:45 +0100

    added switch alternative with handy class pattern.

---
+++ python/switch.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""Switch/case control statement in Python
+
+Also a handy pattern for delegating work based on Class,
+where polymorphism may not be practical.
+"""
+
+
+def do_a(data):
+    print "do_a() with '{0}'".format(data)
+
+
+def do_b(data):
+    print "do_b() with '{0}'".format(data)
+
+
+def do_c(data):
+    print "do_c() with '{0}'".format(data)
+
+
+def do_default(data):
+    print "do_default() with '{0}'".format(data)
+
+switches = {
+    'A': do_a,
+    'B': do_b,
+    'C': do_c
+}
+
+
+class A(object):
+    pass
+
+
+class B(object):
+    pass
+
+
+class C(object):
+    pass
+
+
+class D(object):
+    """No switch for D"""
+    pass
+
+
+a = A()
+b = B()
+c = C()
+d = D()
+
+switches.get(a.__class__.__name__, do_default)('data')
+switches.get(b.__class__.__name__, do_default)('data')
+switches.get(c.__class__.__name__, do_default)('data')
+switches.get(d.__class__.__name__, do_default)('data')
