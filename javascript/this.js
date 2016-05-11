33a5ef94d3739dc54899992e2b5b3d78899829da
David Lavelle <dlavelle7@yahoo.ie>
2016-05-11 19:04:23 +0100

    updated this example.

--- javascript/this.js
+++ javascript/this.js
@@ -1,7 +1,5 @@
-/* 
+/*
    Examples of using 'this'
-
-   Run with nodejs
 */
 
 // TODO: Finish and tidy (JSLint?)
@@ -10,16 +8,20 @@
 // 1. Global context
 this.spam = "eggs";
 
-console.log("Example #1:", this);
+console.log("Example #1:", this["spam"] === "eggs");
 
-// 2.1 Function context
-//function getThis(){
-//    // The value of this not set by call, defaults to global
-//    return this;
-//}
-//
-//// FIXME: This is false in nodejs????
-//console.log(this === getThis())
+// 2 Function context
+// 2.1 Simple call
+function getThis(){
+    // The value of this not set by call, defaults to global
+    return this;
+}
+
+// When used from browser (diff for nodejs)
+if (!(typeof(window) === 'undefined')){
+    console.log("Example #2.1:", this === getThis())
+    console.log("Example #2.1:", window === getThis())
+}
 
 // 2.2 Function context with strict mode
 function strictGetThis(){
@@ -32,12 +34,38 @@ function strictGetThis(){
 console.log("Example #2.2:", strictGetThis() === undefined)
 
 // 2.3 Function context with bind()
-function spam(){
+function camelot(){
     return this.spam;
 }
 
 // Bind creates a new function same as the bound
-// function, but this is the first arg of bind()
-var foo = spam.bind({"spam": "foo"})
+// function, but its 'this' is the first arg of bind()
+var foo = camelot.bind({"spam": "foo"})
 
 console.log("Example #2.3:", foo() === "foo");
+
+// 2.4 An object method
+var obj = {
+    value: "bar",
+    foo: function () {
+        return this.value;  // here 'this' refers to the object
+    }
+}
+
+console.log("Example #2.4:", obj.foo() === "bar");
+
+// 2.5 Using call() and apply()
+function multiply(var1, var2){
+    return this.foo + var1 + var2;
+}
+
+var that = {foo: "foo"};
+
+// The first argument will be the functions 'this' object,
+// the subsequent args will be passed to the function
+var res = multiply.call(that, "bar", "baz");
+console.log("Example#2.5:", res === "foobarbaz");
+
+// apply() works the same, but function arguments are passed as array
+var res = multiply.apply(that, ["bar", "baz"]);
+console.log("Example#2.5:", res === "foobarbaz");
