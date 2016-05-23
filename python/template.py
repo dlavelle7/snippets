cb8717b1f718c38767a00b006f35e0a9c4bff64a
David Lavelle <dlavelle7@yahoo.ie>
2016-05-23 22:12:57 +0100

    Small improvement to template.

--- python/template.py
+++ python/template.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import time
 import sys
+import traceback
 
 
 def main():
     while True:
         print "Interrupt me!"
+        a
         time.sleep(3)
 
 
@@ -16,5 +18,6 @@ if __name__ == "__main__":
         print 'Keyboard Interrupt, exiting . . .'
         sys.exit(1)
     except Exception as e:
+        traceback.print_exc()
         print 'Something went wrong, exiting . . .'
         sys.exit(1)
