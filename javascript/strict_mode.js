246e1db743da655fe522e5b6c8beb1ce0289ab38
David Lavelle <dlavelle7@yahoo.ie>
2016-07-01 19:47:45 +0100

    finish strict mode.

--- javascript/strict_mode.js
+++ javascript/strict_mode.js
@@ -1,12 +1,8 @@
 /*
-    Examples of using JavaScripts 'strict mode'
-
-    Strict mode makes the folowing changes to normal JavaScript behaviour.
-    Strict mode applies to entire scripts or individual functions. Strict mode
-    is declared by using the 'use strict'; literal expression
+    Some examples of how 'strict mode' changes normal JavaScript behaviour.
 */
 
-// 1. Strict mode for scripts
+// 1. Strict mode for entire scripts
 // Make entire script strict mode by placing literal expression at the
 // begining of a script (before any other statements).
 
@@ -14,7 +10,7 @@
 // foo = 'foo'; // This would cause a Reference error (foo not defined)
 
 // 2. Strict mode for functions
-// Put the statement in the functions body before any of its statements invoke
+// Put the statement in the functions body before any of its statements
 function withStrict() {
     'use strict';
     bar = 'bar'; // throws a ReferenceError, bar not defined
@@ -35,8 +31,7 @@ function withoutStrict() {
 
 console.assert(withoutStrict() === 'baz');
 
-// 3. Strict mode differences
-// Silent errors get thrown
+// 3. Silent errors get thrown
 function undeletablePropertiesStrict() {
     'use strict';
     delete Object.prototype;  // throws type error in strict
@@ -50,8 +45,3 @@ catch (error) {
 }
 
 delete Object.prototype;  // fails silently without strict
-
-// TODO:
-// Other differences:
-// function dupNames(foo, foo) { return foo; }
-// "with" keyword is forbidden
