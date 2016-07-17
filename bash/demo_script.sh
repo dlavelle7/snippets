ab526fbbf0260be57294c956acccf71cbc067be4
David Lavelle <dlavelle7@yahoo.ie>
2016-07-17 14:17:21 +0100

    added bash demo script.

---
+++ bash/demo_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Useful bash script for doing live demos
+
+function wait_enter {
+    SZ=`stty size | cut -f1 -d' '`
+    tput cup ${SZ} 0; read -p 'Press enter to continue . . .'
+    clear
+}
+
+function run_and_show_command {
+    CMD=$1
+    echo $CMD
+    printf "\n"
+    eval $CMD
+}
+
+clear
+
+cat << __EOM__
+Demo Heading
+------------
+
+Demo intro text here . . .
+
+__EOM__
+
+wait_enter
+
+cat << __EOM__
+List Files
+----------
+
+e.g list files in working directory:
+
+__EOM__
+
+run_and_show_command "ls -la"
+
+wait_enter
