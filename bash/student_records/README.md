7ad7e5abee324447e51f6ac980377d7496644f01
David Lavelle <dlavelle7@yahoo.ie>
2016-07-19 19:15:24 +0100

    basic question answered.

---
+++ bash/student_records/README.md
@@ -0,0 +1,44 @@
+Student Records
+===============
+
+Question
+--------
+
+All input files will be contained in a folder named input
+
+All output files must be stored to a folder named output, which will exist.
+
+Results for module marks are contained in folders for each block (1-4)
+
+Results take the form
+
+<StudentID><Mark>
+
+and are contained in files named <modulecode>.txt
+
+There is also a file named Students.txt containing:
+
+<StudentID><Surname> <name><Birthdate><Address>
+
+on each line.
+
+You must produce student records for each student. These must be contained in a folder named <StudentID>
+
+There must be 
+
+* a file named Details.txt containing student details
+
+* a file for each block containing results for that block, in the form:
+
+    <ModuleID> <Mark>
+
+* a file named notes.txt, if applicable containing 
+
+    Failed:
+
+    <ModuleID> 
+
+Testing
+-------
+* ./create_records.sh
+* diff -r output/ test/expected/
---
+++ bash/student_records/create_records.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
+# Cleanup old output first
+rm -r $SCRIPT_DIR/output
+mkdir $SCRIPT_DIR/output
+
+
+function write_student_resuts {
+    # Get block name, alternative to: block_name=$(basename $1)
+    strip_slash=${1%/}  # remove trailing slash
+    block_name=${strip_slash##*/}  # remove leading path
+    student_block_path=${2}/${block_name}_results.txt
+    for module in $1*.txt; do
+        # Alternatively: module_name=$(basename $module .txt)
+        module_filename=${module##*/}
+        module_name=${module_filename%.txt}
+        # Find this students results for each module
+        while read -r result_line; do
+            IFS=' ' read -a result_array <<< "$result_line"
+            if [ "${result_array[0]}" = $student_id ]
+            then
+                module_result="${result_array[1]}"
+                # Append module result to this block (double >> appends)
+                echo $module_name $module_result >> $student_block_path
+                # Check if this student has failed
+                check_for_failures $module_result $2 $module_name
+            fi
+        done < $module
+    done
+}
+
+
+function check_for_failures {
+
+    if [ $1 -lt 40 ]
+    then
+        notes_file_path=${2}/notes.txt
+        # If this is the first found failure, create notes.txt
+        if [ ! -f $notes_file_path ]
+        then
+            echo 'Failed:' > $notes_file_path
+        fi
+        # Append the failed module result
+        echo $3 >> $notes_file_path
+    fi
+
+}
+
+
+# Loop over each student
+while read -r student; do
+    # Get next student details
+    IFS=' ' read -a student_array <<< "$student"  # could do: array=($student)
+    student_id="${student_array[0]}"
+
+    # Create output for that student
+    student_dir="${SCRIPT_DIR}/output/$student_id"
+    mkdir $student_dir
+    # Write details for that student (single > writes new)
+    echo "${student}" > $student_dir/Details.txt
+
+    # Loop over the blocks and modules to find this students results
+    for block_path in ${SCRIPT_DIR}/input/*/; do
+        write_student_resuts $block_path $student_dir
+    done
+
+done < "${SCRIPT_DIR}/input/Students.txt"
