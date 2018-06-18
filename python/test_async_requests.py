abdbd526417572e7881b0186b86bdfbff2df7b95
David Lavelle <davidlavelle1@hotmail.com>
2018-06-18 21:08:10 +0100

    unit test done

--- python/test_async_requests.py
+++ python/test_async_requests.py
@@ -7,12 +7,28 @@ class TestAsyncRequests(TestCase):
 
     @patch('async_requests.ClientSession.__aenter__')
     async def test_make_async_requests(self, mock_session_context):
-        mock_get_json = CoroutineMock(side_effect=[{"count": 1, "id": 111}])
+        mock_get_json = CoroutineMock(
+            side_effect=[
+                {"count": 1, "id": 1},
+                {"count": 1, "id": 2},
+                {"count": 1, "id": 3},
+            ]
+        )
         mock_get_response = MagicMock()
+        # async context managers utilize __aenter__()
         mock_get_response.__aenter__.return_value = MagicMock(
             json=mock_get_json)
+
         mock_get = MagicMock(return_value=mock_get_response)
         mock_session = MagicMock(get=mock_get)
         mock_session_context.return_value = mock_session
+
         actual = await make_async_requests(['foo', 'bar', 'spam'])
-        # TODO: Assert mock call args . . .
+
+        # Assert mock get called with expected urls
+        expected_calls = [
+            call('foo', params={}),
+            call('bar', params={}),
+            call('spam', params={})
+        ]
+        self.assertEqual(expected_calls, mock_get.call_args_list)
