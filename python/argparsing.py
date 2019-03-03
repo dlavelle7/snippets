130444daec2c116d28bc1f88f6bbe366d25c3dd8
David Lavelle <davidlavelle1@hotmail.com>
2019-03-03 12:44:08 +0000

    flake8 argparse example

--- python/argparsing.py
+++ python/argparsing.py
@@ -8,17 +8,18 @@ def main():
     # Positional argument
     parser.add_argument("base", help="base value", type=int)
     # Optional argument
-    parser.add_argument("-e", "--expodent", help="expodent value (defaults to 2)",
-            type=int, default=2, choices=[0, 1, 2, 3, 4])
+    parser.add_argument("-e", "--expodent",
+                        help="expodent value (defaults to 2)", 
+                        type=int, default=2, choices=[0, 1, 2, 3, 4])
     group.add_argument("-v", "--verbose", help="increased output verbosity",
-            action="store_true")
+                       action="store_true")
     group.add_argument("-q", "--quiet", help="minimum output verbosity",
-            action="store_true")
+                       action="store_true")
     args = parser.parse_args()
-    answer = args.base**args.expodent
+    answer = args.base ** args.expodent
     if args.verbose:
         print "{0} to the power of {1} equals {2}".format(
-                args.base, args.expodent, answer)
+            args.base, args.expodent, answer)
     elif args.quiet:
         print answer
     else:
